72ae479ac8f75e434d1f93d3dde48a3ff0bcba2c
Mohamed <mohamedhadodo33@gmail.com>
2025-09-13 20:06:36 +0100

    Fix: file permissions in bonus

--- pipex_bonus_utils.c
+++ pipex_bonus_utils.c
@@ -24,11 +24,11 @@ int	open_file(char *file, int in_or_out)
 	int	ret;
 
 	if (in_or_out == 0)
-		ret = open(file, O_RDONLY, 0777);
+		ret = open(file, O_RDONLY, 0644);
 	if (in_or_out == 1)
-		ret = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0777);
+		ret = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (in_or_out == 2)
-		ret = open(file, O_WRONLY | O_CREAT | O_APPEND, 0777);
+		ret = open(file, O_WRONLY | O_CREAT | O_APPEND, 0644);
 	if (ret == -1)
 		exit(0);
 	return (ret);
